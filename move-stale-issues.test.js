d8622b2f1a467fc7341633e7c0bd1591eca94224
Deividy Metheler Zachetti <deividyz@gmail.com>
2022-01-21 14:16:21 -0300

    add test case for exempt-all-assignees

--- move-stale-issues.test.js
+++ move-stale-issues.test.js
@@ -4,6 +4,10 @@ const github = require('@actions/github');
 const assert = require('assert');
 
 describe('Move Stale Issues', () => {
+    const token = process.env.REPO_TOKEN;
+    const repo = process.env.REPO_NAME;
+    const owner = process.env.REPO_OWNER;
+
     it('getDaysDiff', () => {
         const now = new Date();
         const nextDay = new Date();
@@ -20,11 +24,7 @@ describe('Move Stale Issues', () => {
         assert.strictEqual(getDaysDiff(now, oneWeek), -7);
     });
 
-    it('runAction', async() => {
-        const token = process.env.REPO_TOKEN;
-        const repo = process.env.REPO_NAME;
-        const owner = process.env.REPO_OWNER;
-
+    it('runAction with exempt-all-assignees false', async() => {
         const fromMilestone = `From Test Milestone ${new Date().getTime()}`;
         const targetMilestone = `Target Test Milestone ${new Date().getTime()}`;
 
@@ -94,6 +94,95 @@ describe('Move Stale Issues', () => {
         assert.strictEqual(issuesInFrom.length, 0);
 
         for (const issue of issuesInTarget) {
+            await octokit.rest.issues.update({
+                owner,
+                repo,
+                issue_number: issue.number,
+                state: 'closed'
+            });
+
+        }
+    });
+
+    it('runAction with exempt-all-assignees true', async() => {
+        const fromMilestone = `From Test Milestone ${new Date().getTime()}`;
+        const targetMilestone = `Target Test Milestone ${new Date().getTime()}`;
+
+        const octokit = github.getOctokit(token);
+
+        await octokit.rest.issues
+            .createMilestone({ owner, repo, title: fromMilestone });
+
+        await octokit.rest.issues
+            .createMilestone({ owner, repo, title: targetMilestone });
+
+        const { data: milestones } = await octokit.rest.issues.listMilestones({
+            owner,
+            repo,
+            state: 'open'
+        });
+
+        const fromMilestoneResponse = milestones
+            .find((m) => m.title === fromMilestone);
+
+        const targetMilestoneResponse = milestones
+            .find((m) => m.title === targetMilestone);
+
+        await octokit.rest.issues.create({
+            owner,
+            repo,
+            title: 'Test case issue',
+            milestone: fromMilestoneResponse.number,
+            assignees: [ 'Deividy' ]
+        });
+
+        await octokit.rest.issues.create({
+            owner,
+            repo,
+            title: 'Test case issue',
+            milestone: fromMilestoneResponse.number
+        });
+
+        await runAction({
+            token,
+            fromMilestone,
+            targetMilestone,
+            daysBeforeStale: 0,
+            exemptAllAssignees: true
+        }, { repo, owner });
+
+        await octokit.rest.issues.updateMilestone({
+            owner,
+            repo,
+            state: 'closed',
+            milestone_number: fromMilestoneResponse.number
+        });
+
+        await octokit.rest.issues.updateMilestone({
+            owner,
+            repo,
+            state: 'closed',
+            milestone_number: targetMilestoneResponse.number
+        });
+
+        const { data: issuesInTarget } = await octokit.rest.issues.listForRepo({
+            owner,
+            repo,
+            state: 'open',
+            milestone: targetMilestoneResponse.number
+        });
+
+        const { data: issuesInFrom } = await octokit.rest.issues.listForRepo({
+            owner,
+            repo,
+            state: 'open',
+            milestone: fromMilestoneResponse.number
+        });
+
+        assert.strictEqual(issuesInTarget.length, 1);
+        assert.strictEqual(issuesInFrom.length, 1);
+
+        for (const issue of issuesInFrom.concat(issuesInTarget)) {
             await octokit.rest.issues.update({
                 owner,
                 repo,
