67d6f5e848d274abfb32e7c3081a4bdad4892c5e
Deividy Metheler Zachetti <deividyz@gmail.com>
2022-01-24 18:43:33 -0300

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
+    entry: './github-action.js',
     mode: 'production',
     target: 'node',
     node: false,
     output: {
-        filename: 'move-stale-issues.js',
+        filename: 'github-action.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    optimization: {
+        minimizer: [ new TerserPlugin({ extractComments: false }) ],
+    },
 };
 
