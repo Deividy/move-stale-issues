0a97f71824fef86a76b037f33097322690585bd4
Deividy Metheler Zachetti <deividyz@gmail.com>
2022-11-23 08:34:41 -0300

    add option to close issues in target milestone if stale for days

--- move-stale-issues.js
+++ move-stale-issues.js
@@ -34,7 +34,8 @@ async function runAction(owner, repo, config) {
         fromMilestone,
         targetMilestone,
         daysBeforeStale = 30,
-        exemptAllAssignees = false
+        exemptAllAssignees = false,
+        closeIssuesInTargetIfStaleForDays = false
     } = config;
 
     logger.info('Starting action with params:');
@@ -42,6 +43,9 @@ async function runAction(owner, repo, config) {
     logger.info(`\ttargetMilestone: ${targetMilestone}`);
     logger.info(`\tdaysBeforeStale: ${daysBeforeStale}`);
     logger.info(`\texemptAllAssignees: ${exemptAllAssignees}`);
+    logger.info('\tcloseIssuesInTargetIfStaleForDays: ' +
+        closeIssuesInTargetIfStaleForDays);
+
     logger.info(`\trepo owner: ${owner}`);
     logger.info(`\trepo: ${repo}`);
 
@@ -124,8 +128,54 @@ async function runAction(owner, repo, config) {
         totalMoved++;
     }
 
+    let totalClosed = 0;
+    if (closeIssuesInTargetIfStaleForDays !== false) {
+        const fn = octokit.rest.issues.listForRepo;
+        const issuesToClose = [];
+
+        for await (const response of octokit.paginate.iterator(fn, {
+            owner,
+            repo,
+            state: 'open',
+            per_page: 100,
+            milestone: targetMilestoneId
+        })) {
+            const issues = response.data;
+
+            for (const issue of issues) {
+                const { number, updated_at, pull_request } = issue;
+                if (pull_request) { continue; }
+
+                const issueDate = new Date(updated_at);
+                const diff = getDaysDiff(now, issueDate);
+
+                logger.info(`\tLast update on issue #${number} was ${diff} days ago.`);
+                if (diff >= closeIssuesInTargetIfStaleForDays) {
+                    logger.info(`\t[stale] {close} Issue #${number} is stale.`);
+                    issuesToClose.push(issue);
+                } else {
+                    logger.info(`\t[not stale] Issue #${number} is NOT stale.`);
+                }
+            }
+
+            for (const issue of issuesToClose) {
+                const { number } = issue;
+                await octokit.rest.issues.update({
+                    owner,
+                    repo,
+                    issue_number: number,
+                    state: 'closed'
+                });
+
+                logger.info(`\t[stale] Issue #${number} closed!`);
+                totalClosed++;
+            }
+        }
+    }
+
     logger.info('Done!');
-    logger.info(`Total of issues moved: ${totalMoved}.`);
+    logger.info(`Total of issues moved: ${totalMoved}, ` +
+        `total closed: ${totalClosed}.`);
 }
 
 module.exports = { logger, getDaysDiff, runAction };
